e32791690648668ab2b7f1004acf7bcdcbd89095
antonio francisco <antoniofco20142014@gmail.com>
2023-08-25 11:26:45 -0300

    atualização

--- aplicativo.py
+++ aplicativo.py
@@ -1,11 +1,10 @@
 import tkinter as tk
 import folium
-import io
 import geocoder
 import math
 import psycopg2
 from decimal import Decimal
-from tkhtmlview  import HTMLLabel
+from tkhtmlview import HTMLLabel
 
 dbname = "sus"
 user = "antoniofco"
@@ -16,76 +15,75 @@ port = "5432"
 connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
 cursor = connection.cursor()
 
+def calcular_distancia_haversine(coord1, coord2):
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+    R = 6371.0  # Raio médio da Terra em quilômetros
+
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+
+    a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    distance = R * c
+    return distance
+
 def plotar_mapa():
-	cont = 0;
-	html_content= "";
-	localizacoes_proximas = locationsdistancia();
-	for loc in localizacoes_proximas:
-		latitude1, longitude1 = loc[4],loc[5]
-		latitude2, longitude2 = loc[1],loc[2]
-		#print(latitude1," ",longitude1)
-		#print(latitude2," ",longitude2)
+    cont = 0
+    html_content = ""
+    localizacoes_proximas = locationsdistancia()
+    for loc in localizacoes_proximas:
+        latitude1, longitude1 = loc[4], loc[5]
+        latitude2, longitude2 = loc[1], loc[2]
 
-		mapa = folium.Map(location=[latitude1, longitude1], zoom_start=15)
+        mapa = folium.Map(location=[latitude1, longitude1], zoom_start=15)
 
-		folium.Marker([latitude1, longitude1], tooltip='Minha Localização').add_to(mapa)
-		folium.Marker([latitude2, longitude2], tooltip='Alvo da Localização').add_to(mapa)
+        folium.Marker([latitude1, longitude1], tooltip='Minha Localização').add_to(mapa)
+        folium.Marker([latitude2, longitude2], tooltip='Alvo da Localização').add_to(mapa)
 
-		linha = folium.PolyLine([(latitude1, longitude1), (latitude2, longitude2)], color='blue')
-		linha.add_to(mapa)
+        linha = folium.PolyLine([(latitude1, longitude1), (latitude2, longitude2)], color='blue')
+        linha.add_to(mapa)
 
-		temp_map_file = "temp_map_"+str(cont)+".html"
-		#print(temp_map_file)
-		mapa.save(temp_map_file)
-		nome = loc[0]
-		html_content += "<a href='temp_map_"+str(cont)+".html'>Vá para o "+str(nome)+"</a><br>"
-		cont+=1
-	my_label = HTMLLabel(janela, html=html_content)
-	my_label.pack(pady=20, padx=20)
-	
-	cursor.close()
-	connection.close()
+        temp_map_file = "temp_map_" + str(cont) + ".html"
+        mapa.save(temp_map_file)
+        nome = loc[0]
+        html_content += "<a href='temp_map_" + str(cont) + ".html'>Vá para o " + str(nome) + "</a><br>"
+        cont += 1
+    my_label = HTMLLabel(janela, html=html_content)
+    my_label.pack(pady=20, padx=20)
+
+    cursor.close()
+    connection.close()
 
 def locationsdistancia():
-	g = geocoder.ip('me')
-	if g.latlng:
-		latitude, longitude = g.latlng
-		if latitude and longitude:
-			print(f"Latitude: {latitude}, Longitude: {longitude}")
-	else:
-		print("Não foi possível obter a localização.")
+    g = geocoder.ip('me')
+    if g.latlng:
+        latitude, longitude = g.latlng
+        minha_localizacao = (latitude, longitude)
+    else:
+        print("Não foi possível obter a localização.")
+        return []
 
-	minha_localizacao = (latitude, longitude)
+    query = "SELECT CO_CNES, NU_LATITUDE, NU_LONGITUDE, NO_FANTASIA FROM locais_atendimento"
+    cursor.execute(query)
+    all_locations = cursor.fetchall()
 
-	query = "SELECT CO_CNES, NU_LATITUDE, NU_LONGITUDE, NO_FANTASIA FROM locais_atendimento"
-	cursor.execute(query)
-	all_locations = cursor.fetchall()
+    distancias = []
+    for loc in all_locations:
+        localizacao_bd = (float(loc[1]), float(loc[2]))
+        distancia = calcular_distancia_haversine(minha_localizacao, localizacao_bd)
+        distancias.append((loc[3], loc[1], loc[2], distancia, latitude, longitude))
 
-	distancias = []
-	for loc in all_locations:
-		#print(loc[0], " ",loc[1], " ",loc[2])
-		localizacao_bd = (float(loc[1]), float(loc[2]))  # Convertendo Decimal para float
-		distancia = calcular_distancia_euclidiana(minha_localizacao, localizacao_bd)
-		distancias.append((loc[3], loc[1], loc[2], distancia, latitude, longitude))
+    distancias.sort(key=lambda x: x[3])
 
-	distancias.sort(key=lambda x: x[1])
-
-	localizacoes_proximas = distancias[:10]
-	return localizacoes_proximas
-
-
-def calcular_distancia_euclidiana(coord1, coord2):
-	lat1, lon1 = coord1
-	lat2, lon2 = coord2
-	return math.sqrt((lat1 - lat2)**2 + (lon1 - lon2)**2)
+    localizacoes_proximas = distancias[:10]
+    return localizacoes_proximas
 
 janela = tk.Tk()
 janela.title("Mapa com Coordenadas")
 
-
 botao = tk.Button(janela, text="Me mostre os Mapas", command=plotar_mapa)
 botao.pack()
-
-
 
 janela.mainloop()
--- upload.py
+++ upload.py
@@ -7,40 +7,77 @@ password = "ck5XhvZgOw3q"
 host = "ep-sweet-grass-79413934.us-east-2.aws.neon.tech"
 port = "5432"
 
+# Your connection details here
+
 csv_file = "cnes_estabelecimentos.csv"
+batch_size = 1000
 columns_to_read = ["CO_CNES", "NO_FANTASIA", "NU_LATITUDE", "NU_LONGITUDE"]
+progress_file = "progress.txt"  # File to store progress information
+
+connection = None
 
 try:
-	connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-	cursor = connection.cursor()
+    connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+    cursor = connection.cursor()
 
-	with open(csv_file, 'r', encoding="utf-8") as file:
-		next(file)  # Pula a linha de cabeçalho
-		for line in file:
-			data = line.strip().split(";")
-			co_cnes = data[0]
-			no_fantasia = data[6]
-			nu_latitude = data[21]
-			nu_longitude = data[22]
-			#print(nu_latitude, ' ', nu_longitude)
-			#print(nu_latitude=="")
-			#print(pd.notna(nu_latitude))
-			if pd.notna(nu_latitude) and pd.notna(nu_longitude)  and nu_latitude!="" and nu_longitude!="":
-				select_query = f"SELECT co_cnes FROM locais_atendimento WHERE co_cnes = {co_cnes}"
-				cursor.execute(select_query)
-				existing_record = cursor.fetchone()
+    # Load progress if available
+    try:
+        with open(progress_file, 'r') as progress:
+            last_processed = int(progress.read())
+    except FileNotFoundError:
+        last_processed = 0
 
-				if not existing_record:
-					insert_query = f"INSERT INTO locais_atendimento(co_cnes, no_fantasia, nu_latitude, nu_longitude) VALUES ({co_cnes}, '{no_fantasia}', {nu_latitude}, {nu_longitude})"
-					cursor.execute(insert_query)
-					connection.commit()  # Inserção imediata
+    with open(csv_file, 'r', encoding="latin-1") as file:
+        next(file)
+        inserted_count = last_processed
 
-	print("Dados inseridos com sucesso")
+        batch_data = []
+
+        for idx, line in enumerate(file):
+            if idx < last_processed:
+                continue
+
+            data = line.strip().split(";")
+            co_cnes = data[0]
+            no_fantasia = data[6]
+            nu_latitude = data[21]
+            nu_longitude = data[22]
+
+            if pd.notna(nu_latitude) and pd.notna(nu_longitude) and nu_latitude != "" and nu_longitude != "":
+                select_query = f"SELECT co_cnes FROM locais_atendimento WHERE co_cnes = {co_cnes}"
+                cursor.execute(select_query)
+                existing_record = cursor.fetchone()
+
+                if not existing_record:
+                    batch_data.append((co_cnes, no_fantasia, nu_latitude, nu_longitude))
+                    inserted_count += 1
+
+                    if len(batch_data) >= batch_size:
+                        insert_query = "INSERT INTO locais_atendimento(co_cnes, no_fantasia, nu_latitude, nu_longitude) VALUES (%s, %s, %s, %s)"
+                        cursor.executemany(insert_query, batch_data)
+                        connection.commit()
+                        print(f"Inserted {inserted_count} records")
+
+                        # Save progress
+                        with open(progress_file, 'w') as progress:
+                            progress.write(str(idx + 1))  # +1 to account for 0-based index
+
+                        # Clear batch data for the next batch
+                        batch_data = []
+
+        # Insert any remaining records in the last batch
+        if batch_data:
+            insert_query = "INSERT INTO locais_atendimento(co_cnes, no_fantasia, nu_latitude, nu_longitude) VALUES (%s, %s, %s, %s)"
+            cursor.executemany(insert_query, batch_data)
+            connection.commit()
+            print(f"Inserted {inserted_count} records")
+
+    print("Data insertion completed")
 
 except (Exception, psycopg2.Error) as error:
-	print("Erro ao conectar ou inserir dados:", error)
+    print("Error connecting or inserting data:", error)
 finally:
-	if connection:
-		cursor.close()
-		connection.close()
-		print("Conexão fechada.")
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
